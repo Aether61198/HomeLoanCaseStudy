3754a0b7f0457dd42ebaa52100bc0b3095ddab3c
Nilay Saxena <nilaysaxena98@gmail.com>
2022-04-26 22:14:34 +0530

    Changed database attributes

--- HomeLoanCaseStudy/Models/User.cs
+++ HomeLoanCaseStudy/Models/User.cs
@@ -13,21 +13,23 @@ namespace HomeLoanCaseStudy.Models
         public int Id { get; set; } //PRIMARY KEY
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public long PhoneNumber { get; set; }
-        public string EmailAdress { get; set; }
+        public long? PhoneNumber { get; set; }
+        [Required]
+        public string EmailAddress { get; set; }
+        [Required]
         public string Password { get; set; }
         public string Gender { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
 
         // DETAILS RELATED TO THE LOAN APPLICATION
-        public int MaxLoanAmount { get; set; } // MAX AMOUNT OF LOAN THE USER CAN AVAIL
+        public int? MaxLoanAmount { get; set; } // MAX AMOUNT OF LOAN THE USER CAN AVAIL
         public string PanNumber { get; set; }
-        public double MonthlySalary { get; set; }
+        public double? MonthlySalary { get; set; }
         public string EmployementType { get; set; }
-        public int RetirementAge { get; set; }
+        public int? RetirementAge { get; set; }
         public string OrganizationType { get; set; }
         public string EmployerName { get; set; }
-        public int AccountNumber { get; set; } // GENERATED AFTER THE USER'S LOAN IS APPROVED
+        public int? AccountNumber { get; set; } // GENERATED AFTER THE USER'S LOAN IS APPROVED
 
         // ASSOCIATION WITH LOAN TABLE
         [Required]
